cc0b01e22ae5e0c5fdfcf5f3e814d6323e7eecb3
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-01 21:44:03 -0800

    Add files via upload
    Practicing arras and foreach

---
+++ ArraysDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpFundamentalsArrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new int[5];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[4] { "Fabio", "Nicole", "Luna", "Bilbo" };
+            foreach(string name in names)
+                Console.WriteLine(name);
+        }
+    }
+}
