98a07f2f07247c1e899f8877d0a528daae661168
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-03 15:05:08 -0800

    Add files via upload
    Just quick exercise about strings

---
+++ StringPractices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpFundamentalsArrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstName = "Fabio";
+            var lastName = "Faria";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(myFullName);
+
+            var names = new String[4] { "Fabio", "Nicole", "Luna", "Bilbo" };
+
+            var formattedNames = string.Join(", ", names);
+
+            var text = "Hi, my name is Fabio and i'm learning all about strings.\nYou should understand this text.\n And it should have few lines";
+            var textVerb = @"Hi, my name is Fabio and i'm learning all about strings.
+                             You should understand this text.
+                             And it should have few lines.
+                             But now we're using verbatum strings. Much better!";
+            Console.WriteLine(textVerb);
+
+        }
+    }
+}
