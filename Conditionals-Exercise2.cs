e8797ac29abbdcf2c8265ad91d9e20d91fb1fc3e
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-10 23:28:34 -0800

    Another conditional exercise
    Another simple exercise requiring two inputs and a conditional to check and return the highest number

---
+++ Conditionals-Exercise2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pick up a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick up another number");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            if(numberOne > numberTwo)
+            {
+                Console.WriteLine(numberOne);
+            }
+            else
+            {
+                Console.WriteLine(numberTwo);
+            }
+
+            
+            
+           
+        }
+
+        
+    }
+}
