5cf1757d5e19ae411481f830350e293f928a9ea0
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-10 23:32:56 -0800

    Add files via upload

---
+++ LandscapePortrait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see if your photo is a Landscape or Portrait.");
+            Console.WriteLine("Height of the photo:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("width of the photo:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            if(height > width)
+            {
+                Console.WriteLine("Your photo is a landscape!");
+            }
+            else
+            {
+                Console.WriteLine("Your photo is a portrait!");
+            }
+            
+        }
+
+        
+    }
+}
