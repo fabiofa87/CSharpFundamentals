9f70e1ae514d8bca4fa090cac01ff850ef15e8f4
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-10 23:16:10 -0800

    Adding an exercise
    Exercise about conditionals requiring an input to validate a simple if/else condition

---
+++ Conditionals-Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 10.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number <= 10 && number >= 1)
+            {
+                Console.WriteLine("Your number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+           
+        }
+
+        
+    }
+}
