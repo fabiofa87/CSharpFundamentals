c5e604d893b6144d6c0f12c6972eda03f3d5998b
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-12 21:46:43 -0800

    Add files via upload
    This is a simple camera to check speed limit from an user.
    
    If you exceed speed limit by 5km/h(mph) you should be penalized by 1 point in your driver license.
    If you're under speed limit, you'll be good to go.

---
+++ CameraSpeedCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpeedCamera
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Whats your speed limit?");
+            int speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Whats your speed now?");
+            int speedNow = Convert.ToInt32(Console.ReadLine());
+
+            if (speedNow < speedLimit)
+            {
+                Console.WriteLine("Youre good.");
+            }
+
+            else
+            {
+                const int kmPerPoints = 5;
+                var points = (speedNow - speedLimit) / kmPerPoints;
+                if (kmPerPoints > 12)
+                {
+                    Console.WriteLine("Your license is suspended!");
+                }
+                else
+                {
+                    Console.WriteLine("Demerit Points: " + points);
+                }
+            }
+
+
+
+
+        }
+    }
+}
