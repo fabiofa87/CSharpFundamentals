e223cbba9d7c497940dc33badffebd29c13261e1
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-16 21:17:38 -0800

    Add files via upload

---
+++ While-Interations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Interations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while(true){
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                        break;
+                }
+
+                continue;
+
+                
+            }
+        }
+    }
+}
