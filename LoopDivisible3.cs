22f12c897990dfa8e3c9d3257ad954f4b9c5dab5
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-16 22:03:32 -0800

    Add files via upload

---
+++ LoopDivisible3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DivisibleLoop
+
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for(var i = 0; i < 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
