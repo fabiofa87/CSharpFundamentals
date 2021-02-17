bd82b0592d0c736b9fd5986942c03f812177b4a1
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-16 22:01:03 -0800

    Add files via upload

---
+++ RandomClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+
+            var password = new char[passwordLength];
+            for(var i = 0; i < passwordLength; i++)
+            
+                password[i] = (char)('a' + random.Next(0, 26));
+
+                var newPassword = new string(password);
+
+                Console.Write(newPassword);
+                
+            
+            
+        }
+    }
+}
