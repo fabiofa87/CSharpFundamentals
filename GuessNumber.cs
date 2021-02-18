df48e70ec3daf5638a11a7ce73babe722c7c7aa4
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-17 21:53:06 -0800

    Add files via upload
    The objective of this exercise is merely pick a number and match with a random number generated by a random class.

---
+++ GuessNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessNumberExercise
+
+{
+    class GuessNumber
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            var rdNumber = random.Next(1, 10);
+
+            for(var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Pick the correct number: ");
+                if (Convert.ToInt32(Console.ReadLine()) == rdNumber)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            //Console.WriteLine(rdNumber);
+        }
+
+        
+    }
+}
