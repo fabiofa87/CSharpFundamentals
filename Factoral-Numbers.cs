ea6674aa52dfea965962d1b3f1a2de2573b462cd
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-17 21:01:00 -0800

    Add files via upload

---
+++ Factoral-Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DivisibleLoop
+
+{
+    class FactoralNumbers
+    {
+        public static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Pick a Number: ");
+            Int32.TryParse(Console.ReadLine(), out number);
+            Console.WriteLine("Factoral number of " + number + " is : " + Factorial(number));
+
+            
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+    }
+}
