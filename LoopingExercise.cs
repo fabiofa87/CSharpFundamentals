ddc1c310991e006df9d5952195e0403ae74d21ca
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-17 20:44:38 -0800

    Add files via upload

---
+++ LoopingExercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DivisibleLoop
+
+{
+    class LoopExercise
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                int sum = 0;
+                while (true)
+                {
+                    Console.WriteLine("Give me a number: ");
+                    int n;
+                    if (int.TryParse(Console.ReadLine(), out n))
+                    {
+                        if (n == 0)
+                            break;
+                        sum += n;
+                    }
+                }
+                Console.WriteLine("The sum is: " +  sum);
+                break;
+                
+            }
+        }
+    }
+}
