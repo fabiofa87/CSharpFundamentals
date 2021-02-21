4269733693ff81d575a40ddb082ee633198a4f7e
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-02-20 22:39:56 -0800

    Add files via upload
    This exercise consist in asking the user a name and storing into a List.
    If the user answer a name, it will keep asking another name. If the user don't answer anything it will stop and show how many names are inside the List.

---
+++ AskName.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace AskNameExercise
+
+{
+    class AskName
+    {
+        public static void Main(string[] args)
+        {
+            var names = new List<string> { };
+
+            while (true)
+            {
+                System.Console.WriteLine("Give me a name or nothing");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    names.Add(input);
+                    continue;
+                }
+                else
+                    break;
+            }
+            foreach (var name in names)
+            {
+
+                Console.WriteLine(name);
+                    Console.WriteLine();
+
+            }
+
+            Console.WriteLine("You gave me: " + names.Count + " names");
+
+        }
+
+        
+    }
+}
