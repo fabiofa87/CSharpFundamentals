20d44fa2c6f947c4070f2c9048b5271cfef71d33
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-09 15:23:12 -0300

    Add files via upload

---
+++ ReverseName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ReversedNameInput
+
+{
+    class AskName
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?");
+            var input = Console.ReadLine();
+
+            var reversedName = new string(input.Reverse().ToArray());
+            Console.WriteLine(reversedName);
+        }
+
+
+    }
+}
