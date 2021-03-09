2797ea32bddfed1daa0da89353c90d5c47d9025b
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-09 15:01:23 -0300

    Update AskName.cs
    Refactored and added a "like" system

--- AskName.cs
+++ AskName.cs
@@ -23,18 +23,29 @@ namespace AskNameExercise
                 else
                     break;
             }
-            foreach (var name in names)
+
+            if(names.Count > 2)
             {
-
-                Console.WriteLine(name);
-                    Console.WriteLine();
-
+                Console.WriteLine("{0}, {1}, and {2} others like your post!", names[0], names[1], names.Count - 2);
             }
+            else if(names.Count == 2)
+            {
+                Console.WriteLine("{0} and {1} like your post.", names[0], names[1]);
+            }
+            else if(names.Count == 1)
+            {
+                Console.WriteLine("{0} likes your post!", names[0]);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
 
             Console.WriteLine("You gave me: " + names.Count + " names");
 
         }
 
-        
+
     }
 }
