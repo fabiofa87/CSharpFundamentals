09625ba1cd90b81ff233b3bc7b6e7f57d04f07d3
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-21 12:35:45 -0300

    Add files via upload

---
+++ UniqueNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace UniqueNumbers
+
+{
+    class UniqueNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            while(true)
+            {
+                Console.WriteLine("Write a number or type quit to exit");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                var uniques = new List<int>();
+                foreach(var number in numbers)
+                {
+                    if (!uniques.Contains(number))
+                        uniques.Add(number);
+                }
+
+                Console.WriteLine("Unique numbers:");
+                foreach(var number in uniques)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            
+        }
+
+
+    }
+}
