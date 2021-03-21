8b429e77883cc79c9f2fea119bb0c567c1ef7717
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-21 12:08:57 -0300

    Add files via upload
    Listing numbers entered into input. if numbers are repeated it'll show at console

---
+++ NumberListed.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ListNumbers
+
+{
+    class NumberListed
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while(numbers.Count < 5)
+            {
+                Console.WriteLine("Enter a number");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("You've previouly entered " + number);
+                    continue;
+                }
+                numbers.Add(number);
+            }
+
+            numbers.Sort();
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+
+    }
+}
